d0ad68e230140a4637058f98949d369dd4122d32
tokio-mori <tokio07181120@gmail.com>
2025-06-04 23:59:41 +0900

    VPC作成

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+}
